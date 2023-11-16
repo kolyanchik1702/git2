1a2fee7cb28581c4cd3f7ddec59242528dd6c263
kolyanchik1702 <45167144+kolyanchik1702@users.noreply.github.com>
2023-11-16 17:19:49 +0300

    My 4th commit

--- program.py
+++ program.py
@@ -1,7 +1,12 @@
 def main():
     print('My first git program')
     print('And I change it every day')
-    print(10)
+    print('Again')
+    print('UFO came and added this line')
+
+
+if __name__ == '__main__':
+    main()
 
 
 if __name__ == '__main__':
